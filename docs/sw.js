d236ae29be1fd339632265534abfc3fdf9559eed
Markciccio <TUA_EMAIL_GITHUB>
2026-03-18 17:39:49 +0100

    Update PWA icons (new logo)

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* Basic offline-first cache for the PWA. */
-const CACHE_NAME = "zerosbatti-cache-v19";
+const CACHE_NAME = "zerosbatti-cache-v20";
 const CORE_ASSETS = [
   "./",
   "./index.html",
