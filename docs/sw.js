673f320e2ff7771284f634fa39886e7a0191d8f0
Markciccio <TUA_EMAIL_GITHUB>
2026-03-26 20:30:29 +0100

    Update app icon set

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* Basic offline-first cache for the PWA. */
-const CACHE_NAME = "zerosbatti-cache-v43";
+const CACHE_NAME = "zerosbatti-cache-v44";
 const CORE_ASSETS = [
   "./",
   "./index.html",
