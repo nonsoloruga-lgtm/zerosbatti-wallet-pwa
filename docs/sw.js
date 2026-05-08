004dd5fe01b4df70de80f21462f25a822ddcb4e0
Markciccio <TUA_EMAIL_GITHUB>
2026-05-08 22:49:01 +0200

    Use Playfair Display for card names

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* Basic offline-first cache for the PWA. */
-const CACHE_NAME = "zerosbatti-cache-v52";
+const CACHE_NAME = "zerosbatti-cache-v53";
 const CORE_ASSETS = [
   "./",
   "./index.html",
